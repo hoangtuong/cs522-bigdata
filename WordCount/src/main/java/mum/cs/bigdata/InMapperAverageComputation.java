004d22b35e9ec55e055cd0ebafe5225be06e741a
TuongHoang <hoangvinhtuong@gmail.com>
2018-01-30 20:47:37 -0800

    Finished average computation

--- WordCount/src/main/java/mum/cs/bigdata/InMapperAverageComputation.java
+++ WordCount/src/main/java/mum/cs/bigdata/InMapperAverageComputation.java
@@ -11,15 +11,16 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.io.ArrayWritable;
 import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 
-public class AverageComputation extends Configured implements Tool {
+public class InMapperAverageComputation extends Configured implements Tool {
 
 	public static void main(String[] args) throws Exception {
-		int res = ToolRunner.run(new AverageComputation(), args);
+		int res = ToolRunner.run(new InMapperAverageComputation(), args);
 		System.exit(res);
 	}
 
@@ -29,13 +30,15 @@ public class AverageComputation extends Configured implements Tool {
 		FileInputFormat.addInputPath(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 		job.setMapperClass(Map.class);
+//		job.setCombinerClass(Combine.class);
 		job.setReducerClass(Reduce.class);
 		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(IntWritable.class);
+		job.setMapOutputValueClass(ArrayWritable.class);
+		job.setOutputValueClass(DoubleWritable.class);
 		return job.waitForCompletion(true) ? 0 : 1;
 	}
 	
-	public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
+	public static class Map extends Mapper<LongWritable, Text, Text, ArrayWritable> {
 	    private Text word = new Text();
 	    
 	    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
@@ -51,7 +54,11 @@ public class AverageComputation extends Configured implements Tool {
 	        	try {
 	        		int number = Integer.parseInt(capacity);
 		        	word.set(ipString);
-		        	context.write(word, new IntWritable(number));
+		        	
+		        	IntWritable[] val = new IntWritable[2];
+		        	val[0] = new IntWritable(number);
+		        	val[1] = new IntWritable(1);
+		        	context.write(word, new ArrayWritable(IntWritable.class, val));
 
 	        	} catch(NumberFormatException ex) {
 	        		
@@ -59,16 +66,33 @@ public class AverageComputation extends Configured implements Tool {
 	        }
 	    }
 	}
+
+//	public static class Combine extends Reducer<Text, TupleWritable, Text, TupleWritable> {
+//    	public void reduce(Text word, Iterable<TupleWritable> pairs, Context context) throws IOException, InterruptedException {
+//    		int sum = 0;
+//    		int count = 0;
+//    		
+//    		for (TupleWritable pair : pairs) {
+//    			IntWritable s = (IntWritable) pair.get(0);
+//    			sum += s.get();
+//    			count += ((IntWritable) pair.get(1)).get();
+//    		}
+//    		
+//    		context.write(word, new TupleWritable(new Writable[] { new IntWritable(sum), new IntWritable(count)}));
+//    	}
+//    }
 	
-	public static class Reduce extends Reducer<Text, IntWritable, Text, DoubleWritable> {
+	public static class Reduce extends Reducer<Text, ArrayWritable, Text, DoubleWritable> {
 		@Override
-		public void reduce(Text word, Iterable<IntWritable> counts, Context context)
+		public void reduce(Text word, Iterable<ArrayWritable> pairs, Context context)
 				throws IOException, InterruptedException {
 			int sum = 0;
 			int cnt = 0;
 			
-			for (IntWritable count : counts) {
-				sum += count.get();
+			for (ArrayWritable pair : pairs) {
+				IntWritable s = (IntWritable) pair.get()[0];
+				sum += s.get();
+//				cnt += ((IntWritable) pair.get(1)).get();
 				cnt ++;
 			}
 			context.write(word, new DoubleWritable(sum/(cnt*1.0)));
