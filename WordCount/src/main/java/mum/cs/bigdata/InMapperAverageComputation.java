449f0457e40b75047ca451c2791e2337a265f929
TuongHoang <hoangvinhtuong@gmail.com>
2018-01-31 14:00:39 -0800

    Finished prf & srf

--- WordCount/src/main/java/mum/cs/bigdata/InMapperAverageComputation.java
+++ WordCount/src/main/java/mum/cs/bigdata/InMapperAverageComputation.java
@@ -2,6 +2,8 @@ package mum.cs.bigdata;
 
 import java.io.IOException;
 
+import mum.cs.bigdata.utils.IntPairWritable;
+
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
@@ -11,14 +13,14 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.io.ArrayWritable;
 import org.apache.hadoop.io.DoubleWritable;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.log4j.Logger;
 
 public class InMapperAverageComputation extends Configured implements Tool {
-
+	private static final Logger logger = Logger.getLogger(WordCount.class);
+	
 	public static void main(String[] args) throws Exception {
 		int res = ToolRunner.run(new InMapperAverageComputation(), args);
 		System.exit(res);
@@ -30,15 +32,15 @@ public class InMapperAverageComputation extends Configured implements Tool {
 		FileInputFormat.addInputPath(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 		job.setMapperClass(Map.class);
-//		job.setCombinerClass(Combine.class);
+		job.setCombinerClass(Combine.class);
 		job.setReducerClass(Reduce.class);
 		job.setOutputKeyClass(Text.class);
-		job.setMapOutputValueClass(ArrayWritable.class);
+		job.setMapOutputValueClass(IntPairWritable.class);
 		job.setOutputValueClass(DoubleWritable.class);
 		return job.waitForCompletion(true) ? 0 : 1;
 	}
 	
-	public static class Map extends Mapper<LongWritable, Text, Text, ArrayWritable> {
+	public static class Map extends Mapper<LongWritable, Text, Text, IntPairWritable> {
 	    private Text word = new Text();
 	    
 	    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
@@ -49,17 +51,14 @@ public class InMapperAverageComputation extends Configured implements Tool {
 	        	String ipString = strs[0];
 	        	String capacity = strs[strs.length-1];
 	        	
-	        	System.out.println("IP: " + ipString + ": " + capacity);
+	        	logger.info("InMapperAverageComputation.Mapper");
 	        	
 	        	try {
 	        		int number = Integer.parseInt(capacity);
 		        	word.set(ipString);
-		        	
-		        	IntWritable[] val = new IntWritable[2];
-		        	val[0] = new IntWritable(number);
-		        	val[1] = new IntWritable(1);
-		        	context.write(word, new ArrayWritable(IntWritable.class, val));
-
+		        	IntPairWritable pair = new IntPairWritable(number, 1);
+		        	logger.info("\n<" + word + ", ( " + pair.getFirst() + ", " + pair.getSecond() + ")>");
+		        	context.write(word, pair);
 	        	} catch(NumberFormatException ex) {
 	        		
 	        	}
@@ -67,33 +66,33 @@ public class InMapperAverageComputation extends Configured implements Tool {
 	    }
 	}
 
-//	public static class Combine extends Reducer<Text, TupleWritable, Text, TupleWritable> {
-//    	public void reduce(Text word, Iterable<TupleWritable> pairs, Context context) throws IOException, InterruptedException {
-//    		int sum = 0;
-//    		int count = 0;
-//    		
-//    		for (TupleWritable pair : pairs) {
-//    			IntWritable s = (IntWritable) pair.get(0);
-//    			sum += s.get();
-//    			count += ((IntWritable) pair.get(1)).get();
-//    		}
-//    		
-//    		context.write(word, new TupleWritable(new Writable[] { new IntWritable(sum), new IntWritable(count)}));
-//    	}
-//    }
+	public static class Combine extends Reducer<Text, IntPairWritable, Text, IntPairWritable> {
+    	public void reduce(Text word, Iterable<IntPairWritable> pairs, Context context) throws IOException, InterruptedException {
+    		int sum = 0;
+    		int count = 0;
+    		
+    		for (IntPairWritable pair : pairs) {
+    			sum += pair.getFirst();
+    			count += pair.getSecond();
+    		}
+    		
+    		context.write(word, new IntPairWritable(sum, count));
+    	}
+    }
 	
-	public static class Reduce extends Reducer<Text, ArrayWritable, Text, DoubleWritable> {
+	public static class Reduce extends Reducer<Text, IntPairWritable, Text, DoubleWritable> {
 		@Override
-		public void reduce(Text word, Iterable<ArrayWritable> pairs, Context context)
+		public void reduce(Text word, Iterable<IntPairWritable> pairs, Context context)
 				throws IOException, InterruptedException {
 			int sum = 0;
 			int cnt = 0;
 			
-			for (ArrayWritable pair : pairs) {
-				IntWritable s = (IntWritable) pair.get()[0];
-				sum += s.get();
-//				cnt += ((IntWritable) pair.get(1)).get();
-				cnt ++;
+			logger.info("InMapperAverageComputation.Reducer");
+			
+			for (IntPairWritable pair : pairs) {
+				logger.info("\n<" + word + ", ( " + pair.getFirst() + ", " + pair.getSecond() + ")>");
+				sum += pair.getFirst();
+				cnt += pair.getSecond();
 			}
 			context.write(word, new DoubleWritable(sum/(cnt*1.0)));
 		}
---
+++ WordCount/src/main/java/mum/cs/bigdata/PairRelativeFrequency.java
@@ -0,0 +1,86 @@
+package mum.cs.bigdata;
+
+import java.io.IOException;
+
+import mum.cs.bigdata.utils.StringPairWritable;
+
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+
+public class PairRelativeFrequency extends Configured implements Tool {
+
+	public static void main(String[] args) throws Exception {
+		int res = ToolRunner.run(new PairRelativeFrequency(), args);
+		System.exit(res);
+	}
+
+	public int run(String[] args) throws Exception {
+		Job job = Job.getInstance(getConf(), "wordcount");
+		job.setJarByClass(this.getClass());
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		job.setMapperClass(Map.class);
+		job.setReducerClass(Reduce.class);
+		job.setOutputKeyClass(StringPairWritable.class);
+		job.setMapOutputValueClass(IntWritable.class);
+		job.setOutputValueClass(DoubleWritable.class);
+		return job.waitForCompletion(true) ? 0 : 1;
+	}
+	
+	public static class Map extends Mapper<LongWritable, Text, StringPairWritable, IntWritable> {
+	    private final static IntWritable ONE = new IntWritable(1);
+	    private final static String STAR = "*";
+	    
+	    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+	        String line = value.toString();
+	        String[] words = line.split(" ");
+	        
+	        String w;
+	        String u;
+	        for (int i = 0; i < words.length; i ++) {
+	        	w = words[i];
+	        	int j = i+1;
+	        	while (j < words.length && !words[j].equals(words[i]) ) {
+	        		u = words[j];
+		        	if (u.trim().length() > 0 && w.trim().length() > 0 && !u.equals(w)) {
+		        		context.write(new StringPairWritable(w, STAR), ONE);
+		        		context.write(new StringPairWritable(w, u), ONE);
+		        	}
+	        		j ++;
+	        	}
+	        }
+	    }
+	}
+	
+	public static class Reduce extends Reducer<StringPairWritable, IntWritable, StringPairWritable, DoubleWritable> {
+	    private final static String STAR = "*";
+	    private int total = 0;
+	    
+		@Override
+		public void reduce(StringPairWritable pair, Iterable<IntWritable> counts, Context context)
+				throws IOException, InterruptedException {
+			
+			int sum = 0;
+			for (IntWritable count : counts) {
+				sum += count.get();
+			}
+			
+			if (pair.getRight().equals(STAR)) {
+				total = sum;
+			} else {
+				context.write(pair, new DoubleWritable(sum*1.0/total));		
+			}
+		}
+	}
+}
