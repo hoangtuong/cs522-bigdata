598dcdc01f8c1420dbc27d5182ed9318088d6948
TuongHoang <hoangvinhtuong@gmail.com>
2018-02-05 12:52:48 -0800

    apply in mapper for pair approach

--- project1/source/src/main/java/bigdata/project1/frequency/PairRelativeFrequency.java
+++ project1/source/src/main/java/bigdata/project1/frequency/PairRelativeFrequency.java
@@ -1,6 +1,10 @@
 package bigdata.project1.frequency;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map.Entry;
 
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.util.Tool;
@@ -16,6 +20,7 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 
+import bigdata.project1.utils.Pair;
 import bigdata.project1.utils.StringPairWritable;
 
 public class PairRelativeFrequency extends Configured implements Tool {
@@ -38,45 +43,53 @@ public class PairRelativeFrequency extends Configured implements Tool {
 		return job.waitForCompletion(true) ? 0 : 1;
 	}
 
-	public static class Map extends
-			Mapper<LongWritable, Text, StringPairWritable, IntWritable> {
-		private final static IntWritable ONE = new IntWritable(1);
+	public static class Map extends Mapper<LongWritable, Text, StringPairWritable, IntWritable> {
 		private final static String STAR = "*";
+		private HashMap<Pair<String, String>, Integer> H = new HashMap<Pair<String, String>, Integer>();
 
-		public void map(LongWritable key, Text value, Context context)
-				throws IOException, InterruptedException {
+		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
 			String line = value.toString();
-			String[] words = line.split(" ");
-
-			String w;
-			String u;
+			List<String> wordList = Arrays.asList(line.split(" "));
+			wordList.removeAll(Arrays.asList(""));
+			String[] words = wordList.toArray(new String[0]);
+			String w, u;
+			
 			for (int i = 0; i < words.length; i++) {
 				w = words[i];
 				int j = i + 1;
+				
 				while (j < words.length && !words[j].equals(words[i])) {
 					u = words[j];
-					if (u.trim().length() > 0 && w.trim().length() > 0
-							&& !u.equals(w)) {
-						context.write(new StringPairWritable(w, STAR), ONE);
-						context.write(new StringPairWritable(w, u), ONE);
-					}
+					putValue(H, new Pair<String, String>(w, STAR), 1);
+					putValue(H, new Pair<String, String>(w, u), 1);
 					j++;
 				}
 			}
 		}
+		
+		@Override
+		protected void cleanup(Context context) throws IOException, InterruptedException {
+			for (Entry<Pair<String, String>, Integer> entry : H.entrySet()) {
+				Pair<String, String> p = entry.getKey();
+				context.write(new StringPairWritable(p.getLeft(), p.getRight()), new IntWritable(entry.getValue()));
+			}
+		}
+		
+		private void putValue(HashMap<Pair<String, String>, Integer> map, Pair<String, String> key, int value) {
+			if (!map.containsKey(key)) {
+				map.put(key, value);
+			} else {
+				map.put(key, map.get(key) + 1);
+			}
+		}
 	}
 
-	public static class Reduce
-			extends
-			Reducer<StringPairWritable, IntWritable, StringPairWritable, DoubleWritable> {
+	public static class Reduce extends Reducer<StringPairWritable, IntWritable, StringPairWritable, DoubleWritable> {
 		private final static String STAR = "*";
 		private int total = 0;
 
 		@Override
-		public void reduce(StringPairWritable pair,
-				Iterable<IntWritable> counts, Context context)
-				throws IOException, InterruptedException {
-
+		public void reduce(StringPairWritable pair, Iterable<IntWritable> counts, Context context) throws IOException, InterruptedException {
 			int sum = 0;
 			for (IntWritable count : counts) {
 				sum += count.get();
@@ -85,7 +98,7 @@ public class PairRelativeFrequency extends Configured implements Tool {
 			if (pair.getRight().equals(STAR)) {
 				total = sum;
 			} else {
-				context.write(pair, new DoubleWritable(sum * 1.0 / total));
+				context.write(pair, new DoubleWritable((float) sum/total));
 			}
 		}
 	}
