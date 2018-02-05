788d15546f4e9c087a92c7e8e7d725160a10b9a2
TuongHoang <hoangvinhtuong@gmail.com>
2018-02-05 15:07:31 -0800

    add 'seconds'

--- project1/report/frequency.sh~
+++ project1/report/frequency.sh~
@@ -7,8 +7,9 @@ hadoop fs -rm -r /user/cloudera/frequency/input
 hadoop fs -mkdir /user/cloudera/frequency /user/cloudera/frequency/input
 hadoop fs -put input/frequency/* /user/cloudera/frequency/input
 
-# Create output folder on local machine
+# Create output folder on local file system
 rm -rf output/frequency
+rm -rf output/time.out.txt
 mkdir output output/frequency
 
 # Run Pair script
--- project1/report/frequency_pair.sh
+++ project1/report/frequency_pair.sh
@@ -2,11 +2,21 @@
 
 set -x
 
-# Remove output file (if existed)
+# Remove output files on both local and HDFS (if existed)
 rm -rf output/frequency/pair.out.txt
+hadoop fs -rm -r /user/cloudera/frequency/output
+
+# For execution time measument
+start=$(date +%s.%N)
 
 # Run Pair Approach algorithms
-hadoop fs -rm -r /user/cloudera/frequency/output
 hadoop jar project1.jar bigdata.project1.frequency.PairRelativeFrequency /user/cloudera/frequency/input /user/cloudera/frequency/output
+
+duration=$(echo "$(date +%s.%N) - $start" | bc)
+printf "Execution time: %.6f seconds" $duration
+echo "Pair: $duration seconds" >> output/time.out.txt
+
+
+# Cat output from HDFS to local file
 hadoop fs -cat /user/cloudera/frequency/output/* > output/frequency/pair.out.txt
 
